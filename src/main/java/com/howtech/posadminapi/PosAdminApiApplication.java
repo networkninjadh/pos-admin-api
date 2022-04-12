54f5678a765940da910168865493c44f2efdf122
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:17:35 -0500

    initial commit

---
+++ src/main/java/com/howtech/posadminapi/PosAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.posadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAdminApiApplication.class, args);
+	}
+
+}
